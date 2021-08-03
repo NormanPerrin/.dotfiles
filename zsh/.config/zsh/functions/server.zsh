4a670c290142e317587cd45c17e9ab8979fe9fdd
Norman Perrin <norman.perrin.94@gmail.com>
2021-08-04 01:17:15 +0200

    Copy into clipboard paste to server url

--- zsh/.config/zsh/functions/server.zsh
+++ zsh/.config/zsh/functions/server.zsh
@@ -1,7 +1,7 @@
 # Credits to https://codemadness.org/paste-service.html for the idea!
 pastesrv() {
 	ssh me "cat > /var/www/paste.nperrin.io/$1"
-	echo "https://paste.nperrin.io/$1"
+	echo "https://paste.nperrin.io/$1" |tr -d '\n' |pbcopy
 }
 
 pastesrv_private() {
